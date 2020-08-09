7323df17e1a18f8ea18b09eba232e6efc4539a70
Jamie Sellars <jamie.sellars@zeroseven.com.au>
2020-08-09 16:12:19 +1000

    Part 9 Done

---
+++ CleanBlog.Core/Helpers/QueryStringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0)
+        {
+            var stringValue = request.QueryString[key];
+
+            if(stringValue != null && !string.IsNullOrEmpty(stringValue) &&
+                int.TryParse(stringValue, out int numericValue))
+            {
+                return numericValue;
+            }
+
+            return fallbackValue;
+
+        }
+    }
+}
